183d31b90267c32157776e1399f046a1bc7e4830
Haziq <fiqri.haziq@gmail.com>
2025-03-19 19:12:30 +0800

    fix link

--- src/components/header-bottom.tsx
+++ src/components/header-bottom.tsx
@@ -70,7 +70,7 @@ export default function HeaderBottom() {
                   </p>
                   <div className="flex flex-col gap-2 mt-2">
                     <Link
-                      href="/our-services/find-a-lawyer"
+                      href="/our-services/find-our-experts"
                       className="text-[#072240] text-sm"
                       onClick={() => setOpen(false)}
                     >
